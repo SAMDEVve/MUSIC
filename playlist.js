08dfc1eadc0a1c0c7c730dfce9deedb1029494ec
SAMDEVve <samfrontendev@gmail.com>
2026-04-25 13:53:52 -0400

    Update playlist.js

--- playlist.js
+++ playlist.js
@@ -3,24 +3,24 @@ const listaCanciones = [
         archivo: "https://www.youtube.com/watch?v=-QBmUGvLwL0&list=PL7pkSK1xbGD6g2_BSXgScugMC1CJkkdBW&index=1&pp=iAQB8AUB",
         nombre: "Think About You",
         autor: "Balynt & floating instance",
-        portada: "img/default.jpg"
+        portada: "default.jpg"
     },
     {
         archivo: "https://www.youtube.com/watch?v=9K-f4rhVXU4&list=PL7pkSK1xbGD6g2_BSXgScugMC1CJkkdBW&index=2&pp=iAQB8AUB0gcJCdQKAYcqIYzv",
         nombre: "sundown.wav",
         autor: "chillity",
-        portada: "img/default.jpg"
+        portada: "default.jpg"
     },
     {
         archivo: "https://www.youtube.com/watch?v=OAWBHnZZMSA&list=PL7pkSK1xbGD6g2_BSXgScugMC1CJkkdBW&index=7&pp=iAQB8AUB0gcJCdQKAYcqIYzv",
         nombre: "Games",
         autor: "Games",
-        portada: "img/default.jpg"
+        portada: "default.jpg"
     },
     {
         archivo: "https://www.youtube.com/watch?v=CPU3nIZrnlU&list=PL7pkSK1xbGD6g2_BSXgScugMC1CJkkdBW&index=9&pp=iAQB8AUB",
         nombre: "Colourful",
         autor: "Spiring",
-        portada: "img/default.jpg"
+        portada: "default.jpg"
     }
 ];
